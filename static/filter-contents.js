271a8fa772e38bed63f3cab468bedd6fc7f8b92d
fishand4ips <alekomegunov@gmail.com>
2020-08-22 00:41:49 +0300

    #44 add icons in contents

--- static/filter-contents.js
+++ static/filter-contents.js
@@ -2,7 +2,7 @@ filterSelection("all")
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("filter-content");
-    if (c == "all") c = "";
+    if (c === "all") c = "";
     for (i = 0; i < x.length; i++) {
         w3RemoveClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
@@ -14,7 +14,7 @@ function w3AddClass(element, name) {
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
     for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+        if (arr1.indexOf(arr2[i]) === -1) {element.className += " " + arr2[i];}
     }
 }
 
@@ -31,9 +31,9 @@ function w3RemoveClass(element, name) {
 }
 
 var btnFilterType = document.getElementById("button-filter-type");
-var tags = btnFilterType.getElementsByClassName("tag-256");
-for (var i = 0; i < tags.length; i++) {
-    tags[i].addEventListener("click", function(){
+var tags_256 = btnFilterType.getElementsByClassName("tag-256");
+for (var i = 0; i < tags_256.length; i++) {
+    tags_256[i].addEventListener("click", function(){
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
