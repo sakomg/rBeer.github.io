62c193e344bdcea39da2cd160b37881f7bc3f007
fishand4ips <alekomegunov@gmail.com>
2020-08-23 20:09:52 +0300

    #61 send response result registr

--- static/subscribe.js
+++ static/subscribe.js
@@ -1,8 +1,13 @@
 function subscribeOnButton() {
-    window._peq.push (['subscribe', 'segment_name', function (res) {
-        if (res.statuscode === 1)
-        {
-            console.log (res);
+    window._peq.push(['subscriber-status', function (res) {
+        if (res.statuscode === 1) {
+            alert("Теперь вы подписаны");
+            console.log(res)
+        } else if (res.statuscode === 2) {
+            alert("Вы уже подписаны")
+        } else {
+            alert("Что-то пошло не так")
         }
-    } ]);
+    }]);
 }
+
